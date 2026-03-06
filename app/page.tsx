3d6b445e68f29fe98729e53876f0db3644bba5d2
masterpranay1 <rajpranaychottu@gmail.com>
2026-03-06 17:25:44 +0530

    feat: Introduce Bhagwat Pad Ashtakam component to the home page and remove dark mode styling.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "./components/Hero";
+import BhagwatPadAshtakam from "./components/BhagwatPadAshtakam";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <BhagwatPadAshtakam />
     </main>
   );
 }
