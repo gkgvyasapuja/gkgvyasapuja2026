8010769e7b648c0f1d55749523d31292b61776f1
masterpranay1 <rajpranaychottu@gmail.com>
2026-03-06 17:57:14 +0530

    feat: Add a new image gallery component and integrate it into the homepage with new image assets.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero";
 import BhagwatPadAshtakam from "./components/BhagwatPadAshtakam";
+import Gallery from "./components/Gallery";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <BhagwatPadAshtakam />
+      <Gallery />
     </main>
   );
 }
