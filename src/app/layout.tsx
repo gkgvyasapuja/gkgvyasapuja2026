7df08794f6f6fa5627dccc164101a6d60e690ef2
Siddharth <siddy2896@gmail.com>
2026-03-26 08:01:27 +0530

    new rule added + import changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 // import { Geist, Geist_Mono } from "next/font/google";
 import "@/app/globals.css";
-import Header from "@/components/Header/Header";
-import Footer from "@/components/Footer/Footer";
-import ScrollToTop from "@/components/ScrollToTop/ScrollToTop";
+import Header from "@components/Header/Header";
+import Footer from "@components/Footer/Footer";
+import ScrollToTop from "@components/ScrollToTop/ScrollToTop";
 import { Toaster } from "sonner";
 
 /* const geistSans = Geist({
@@ -28,9 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`antialiased min-h-screen flex flex-col`}
-      >
+      <body className={`antialiased min-h-screen flex flex-col`}>
         <Header />
         <main className="grow">{children}</main>
         <Toaster />
